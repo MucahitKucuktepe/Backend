2cff732f63e26b543ac77e16513e6a752d14ca9f
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-02-21 21:59:05 +0300

    object Methods

--- clarusway-fs-tr-15-backend-m3/nodejs/class-notes/05-OOP_1/oop1.js
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/05-OOP_1/oop1.js
@@ -68,7 +68,7 @@ const Car = {
 
 console.log(Car.arrowMethod());
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //? ARRAY ESTRUCTURING
 
 const testArray = ['value0', 'value1', 'value2', 'value3', 'value4']
@@ -82,5 +82,76 @@ const testArray = ['value0', 'value1', 'value2', 'value3', 'value4']
 const [var0,var1,var2, ...var9]=testArray
 console.log(var0, var1,var2,var9)
 console.log("merhaba")
+console.log("merhaba")
+console.log("first")
+
+// const newArr=['value5', 'value6'] + testArray
+const newArr=[...testArray, 'value5', 'value6']
+console.log(newArr)
+
+/* ------------------------------------------------------- *
+//?OBJECT DESTRUCTURING
+const Car = {
+
+  brand: 'Ford',
+  model: 'Mustang',
+  year: 1967,
+  isAutoGear: true,
+  colors: ['white', 'red'],
+  details: {
+      color1: 'white',
+      color2: 'red',
+      engineSize: 4900
+  },
+  startEngine: function() {
+      return 'Engine Runned.'
+  }
+}
+
+//? REST OPERATOR (KEY İSİMLERİ ÖNEMLİ)
+const {year, model:newName, brand, ...others}=Car
+console.log(year, newName, brand, others)
+
+//?SPREAD OPERATORU
+const newObj={
+  ...Car,
+  newKey:'new-value'
+}
+console.log(newObj)
+//? Object to JSON
+const json =JSON.stringify(Car)
+console.log(typeof json, json)
+const obj=JSON.parse(json)
+console.log(typeof obj, obj)
+
+
+//! OBJECT TO ARRAY
+const arrKey=Object.keys(Car)
+console.log(arrKey)
+const arrValue=Object.values(Car)
+console.log(arrValue)
+//? Tamamını taşıma yöntemi
+const arrEnt=Object.entries(Car)
+console.log(arrEnt)
+
+/* ------------------------------------------------------- */
+//? CONSTRUCTORS
+// const constructorFunction=function(){
+//   this.property='value'
+// }
+/* ------------------------------------------------------- */
+//? "NEW" KEYWORD
+const carConstructor = function (brand, model, year) {
+  this.brand = brand;
+  this.model = model;
+  this.year = year;
+  this.startEngine = function () {
+    return ("Engine runned");
+  };
+};
+const newCar= new carConstructor('Ford','Mustang',1967)
+console.log(typeof newCar,newCar)
+console.log(newCar.brand)
+console.log(newCar.startEngine())
 
 /* ------------------------------------------------------- */
