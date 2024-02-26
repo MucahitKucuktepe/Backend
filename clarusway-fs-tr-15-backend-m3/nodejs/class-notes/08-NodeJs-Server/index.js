dd5d4fb985f7ef6c6467b343b41391cfb176a705
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-02-26 20:42:37 +0300

    node.js created server

--- clarusway-fs-tr-15-backend-m3/nodejs/class-notes/08-NodeJs-Server/index.js
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/08-NodeJs-Server/index.js
@@ -1,11 +1,19 @@
 "use strict"
 
 //? NodeJs SERVER
-
+require('dotenv').config()
+const PORT=process.env.PORT
+const HOST=process.env.HOST
 // console.log("welcome")
  const http=require("node:http")
- http.createServer((req,res)=>{
+//  http.createServer((req,res)=>{
+//     res.end('<h1>Welcome to Node.js Server</h1>  ')
 
- }).listen(8000)
-
+//  }).listen(8000,()=>console.log(`server runned: http://${HOST}:${PORT}`))
+// }).listen(8000,()=>console.log("server runned: http:127.0.0.1:8000"))
+const app=http.createServer((req,res)=>{
+    res.end('<h1>Welcome to Node.js Server</h1>  ')
+    
+   })
+app.listen(8000,()=>console.log("server runned: http:127.0.0.1:8000"))
  
