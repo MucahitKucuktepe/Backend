63134946152f2c4e0d0112ff56746c4b29b6675d
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-03-06 20:42:05 +0300

    put method

---
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/13-Models_Todo copy/app.js
@@ -0,0 +1,147 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+// Accept json data:
+app.use(express.json());
+require("express-async-errors");
+
+// app.all("/", (req, res) => {
+//   res.send("WELCOME TO TODO API");
+// });
+/* ------------------------------------------------------- */
+
+//MODELS
+
+/* ------------------------------------------------------- */
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("sqlite:./db.sqlite3");
+
+//? define methodu sequelize modeli olusturur:
+//?Her bir model veritabanında bir tabloya denk gelir
+//! sequelize.define("tableName",{ ModelDetails })
+
+const Todo = sequelize.define("todos", {
+  //! İlk sütun olarak id sütunu sequelize tarafından otomatik oluşturulur/yönetilir
+  // id:{
+  //     type: DataTypes.INTEGER,
+  //     allowNull:false, //? default:true
+  //     unique:true, //? default:false
+  //     comment:'description',
+  //     primaryKey:true, //?default:false
+  //     autoIncrement:true, //? default:false
+  //     field:'custom_name',
+  //     defaultValue:'default', //? default: null
+  // }
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: DataTypes.TEXT, // ShortHand Using
+
+  priorty: {
+    // -1:low, 0:Norm, 1:High
+    type: DataTypes.TINYINT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  isDone: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+
+  //? Not need define creatAt & updetedAt fields.
+  //? CretedAt ve updetedAt tanımlamaya gerek yok. Sequelize otomatik olarak oluşturur yönetir.
+});
+
+//!Sycnronization
+//!Model bilgilerini db ' ye uygula
+// sequelize.sync() //* CREATE TABLE İŞLEMİ YAPAR
+// sequelize.sync({force:true}) //* DROP TABLE & CREATE TABLE
+// sequelize.sync({alter:true}) //* TODO BACKUP & DROP  TABLE & CREATE TABLE & FROM BACKUP
+
+//? Connect to db:
+sequelize
+  .authenticate()
+  .then(() => console.log("*DB Connected"))
+  .catch(() => console.log("*DB Not Connected"));
+
+/* ------------------------------------------------------- */
+
+//!****************ROUTERS************//
+
+const router = express.Router();
+
+//LIST TODOS
+router.get("/", async (req, res) => {
+  const data = await Todo.findAndCountAll();
+
+  res.status(200).send({
+    error: false,
+    result: data,
+  });
+});
+
+//! READ TODO
+router.get("/:id", async (req, res) => {
+  // const data = await Todo.findOne({ where: { id: req.params.id } });
+  const data = await Todo.findByPk(req.params.id);
+  res.status(200).send({
+    error: false,
+    result: data,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const receivedData = req.body;
+
+  // const data = await Todo.create({
+  //   title: receivedData.title,
+  //   description: receivedData.description,
+  //   priorty: receivedData.priorty,
+  //   isDone: receivedData.isDone,
+  // });
+
+  const data = await Todo.create({ ...receivedData });
+  res.status(201).send({
+    error: false,
+    result: data.dataValues,
+  });
+});
+//!UPDATE TODO:
+router.put("/:id", async (req, res) => {
+  // const data=Todo.update({...newData}, {...where})
+  const data = await Todo.update(req.body, { where: { id: req.params.id } });
+  res.status(202).send({
+    error:false,
+    message:'updated',
+    rbody:req.body,
+    result:data,
+    new:await Todo.findByPk(req.params.id)
+  })
+});
+app.use(router);
+/* ------------------------------------------------------- */
+const errorHandler = (err, req, res, next) => {
+  const errorStatusCode = res.errorStatusCode ?? 500;
+  console.log("errorHandler worked.");
+  res.status(errorStatusCode).send({
+    error: true, // special data
+    message: err.message, // error string message
+    cause: err.cause, // error option cause
+    // stack: err.stack, // error details
+  });
+};
+app.use(errorHandler);
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
