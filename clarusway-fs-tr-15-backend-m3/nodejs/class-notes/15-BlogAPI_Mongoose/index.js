6974a515f8f8585347d67f1d3d58c86877485481
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-03-09 12:02:08 +0300

    feature: DB connection succesfull

--- clarusway-fs-tr-15-backend-m3/nodejs/class-notes/15-BlogAPI_Mongoose/index.js
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/15-BlogAPI_Mongoose/index.js
@@ -6,14 +6,23 @@ BLOG API WITH MONGOOSE
 
 */
 
+//* npm i express doteenv express express-async-errors
+//* npm i mongoose
+
 const express = require("express");
 const app = express();
+//!DB CONNECTİON
+require('./src/dbConnection')
 
 app.use(express.json); // yukarıda kalmalı
+
+
 
 require("dotenv").config();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
+
+
 
 app.all("/", (req, res) => {
   res.send("WELCOME BLOG API PROJECT");
---
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/15-BlogAPI_Mongoose/src/dbConnection.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//*-----------------------
+
+// EXPRESS - MONGODB CONNECTİON MONGOOSE
+
+const mongoose = require("mongoose");
+
+// mongoose.connect('mongodb://localhost:27017/blogAPI')
+require("dotenv").config();
+const MONGODB = process.env.MONGODB;
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Not Connected"));
