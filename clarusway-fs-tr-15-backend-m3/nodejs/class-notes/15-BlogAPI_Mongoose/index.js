8fc20b02f109797154c728df89e52e573836bc2e
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-03-09 11:35:45 +0300

    feat: error handler created and server running

---
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/15-BlogAPI_Mongoose/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*
+
+BLOG API WITH MONGOOSE 
+
+*/
+
+const express = require("express");
+const app = express();
+
+app.use(express.json); // yukarıda kalmalı
+
+require("dotenv").config();
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+app.all("/", (req, res) => {
+  res.send("WELCOME BLOG API PROJECT");
+});
+
+app.use(require("./src/errorHandler"));
+app.listen(PORT, () =>
+  console.log(`Server Running on  http://${HOST}:${PORT}`)
+);
