5fbadbeea3e3a80c8036f2d3d50d581b88154df7
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-03-21 00:17:13 +0300

    permissions

--- clarusway-fs-tr-15-backend-m3/nodejs/class-notes/20-PersonelApı_password/src/middlewares/permission.js
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/20-PersonelApı_password/src/middlewares/permission.js
@@ -1,5 +1,39 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-// Middleware: permissions
+// Middleware: permissions (authorization)
+
+module.exports = {
+  isLogin: (req, res, next) => {
+    if (req.user && req.user.isActive) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login !");
+    }
+  },
+  isAdmin: (req, res, next) => {
+    if (req.user && req.user.isActive && req.user.isAdmin) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error("NoPermission: You must login and to be admin!");
+    }
+  },
+
+  isLead: (req, res, next) => {
+    const departmentId = req.params?.id;
+    if (
+      req.user &&
+      req.user.isActive &&
+      (req.user.isAdmin ||
+        (req.user.isLead && req.user.departmentId == departmentId))
+    ) {
+      next();
+    } else {
+      res.errorStatusCode = 403;
+      throw new Error('NoPermission: You must login and to be Admin or Department Lead.')
+    }
+  },
+};
