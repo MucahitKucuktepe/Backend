75c6656e2c7410eecad98213254b362e22dd889b
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-03-25 22:39:09 +0300

    user model and controller

--- clarusway-fs-tr-15-backend-m3/nodejs/class-notes/22-PizzaAPI/src/models/user.js
+++ clarusway-fs-tr-15-backend-m3/nodejs/class-notes/22-PizzaAPI/src/models/user.js
@@ -1,6 +1,66 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+//USER MODEL
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      // validate: [
+      //   (password) =>
+      //     /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password),
+      //   "Password type is not correct.",
+      // ],
+      // set: passwordEncrypt,
+      set: (password) => {
+        if (
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password)
+        ) {
+          return passwordEncrypt(password);
+        } else {
+          // res.errorStatusCode = 403;
+          // throw new Error("Password type is not correct.");
+          return "wrong";
+        }
+      },
+      validate: (password) => password != "wrong",
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+      validate: [
+        (email) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email),
+        "Email type is not correct.",
+      ],
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+// Model:
+module.exports = mongoose.model("User", UserSchema);
